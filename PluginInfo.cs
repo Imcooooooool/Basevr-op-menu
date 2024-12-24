0944a1a640840367b7f0afa989c3766d1241a72b
Imcooooooool <131561945+Imcooooooool@users.noreply.github.com>
2024-12-23 16:50:45 -0800

    fixed plugin.cs info to be correct

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,8 +3,8 @@
     public class PluginInfo
     {
         public const string GUID = "org.iidk.gorillatag.iimenu";
-        public const string Name = "ii's Stupid Menu";
-        public const string Description = "Created by @goldentrophy with love <3";
+        public const string Name = "Basevr-op-menu";
+        public const string Description = "Created by @basevr! edited most mods?";
         public const string Version = "5.1.3";
     }
 }
